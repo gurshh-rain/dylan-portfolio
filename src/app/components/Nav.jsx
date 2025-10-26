f2c829c0e321d8de1141306dd95b4a20e257be9b
Gurshaan Gill <gurshaangill@Gurshaans-MacBook-Air-5.local>
2025-10-26 15:13:18 -0400

    dylan-portfolio

--- src/app/components/Nav.jsx
+++ src/app/components/Nav.jsx
@@ -1,19 +1,18 @@
 import Link from "next/link";
 
 const Nav = () => {
-    return (
-        <nav>
-            <div className="nav-logo">
-                <Link href="/">Dylan Ngo</Link>
-            </div>
-            <div className="nav-links">
-                <Link href="/about">About</Link>
-                <Link href="/projects">Projects</Link>
-                <Link href="/contact">Contact</Link>
-
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="nav">
+      <div className="nav-logo">
+        <Link href="/">Dylan Ngo</Link>
+      </div>
+      <div className="nav-links">
+        <Link href="/about">About</Link>
+        <Link href="/projects">Projects</Link>
+        <Link href="/contact">Contact</Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
